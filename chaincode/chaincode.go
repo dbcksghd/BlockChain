b2f498b8c58ae75cd23110a2cdc3bd644ae94604
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2023-07-28 00:39:07 +0900

    Update chaincode.go

--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "github.com/hyperledger/fabric-contract-api-go/contractapi"
-    "log"
-    "math/rand"
-    "strconv"
+	"encoding/json"
+	"fmt"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"log"
+	"math/rand"
+	"strconv"
 )
 
 type SmartContract struct {
-    contractapi.Contract
+	contractapi.Contract
 }
 
 type User struct {
@@ -32,7 +32,7 @@ type QueryResult struct {
 
 func isTrueUserBan(user *User) bool {
 	if user.Ban == true {
-			return true
+		return true
 	}
 	return false
 }
@@ -40,24 +40,24 @@ func isTrueUserBan(user *User) bool {
 func (s *SmartContract) Register(ctx contractapi.TransactionContextInterface, name string, money string, id string) error {
 	userAsBytes1, err := ctx.GetStub().GetState(id)
 	if err != nil {
-			return err
+		return err
 	}
 	user1 := new(User)
 	_ = json.Unmarshal(userAsBytes1, user1)
-	if user1.Name != ""{
+	if user1.Name != "" {
 		return fmt.Errorf("이미 등록된 유저입니다.")
 	}
 	moneyAsInt, _ := strconv.Atoi(money)
 	user := User{
-			LuckyScore: rand.Intn(50),
-			Money:      moneyAsInt,
-			Name:       name,
-			Warning:    2,
-			Ban:        false,
+		LuckyScore: rand.Intn(50),
+		Money:      moneyAsInt,
+		Name:       name,
+		Warning:    2,
+		Ban:        false,
 	}
 	userAsBytes, err := json.Marshal(user)
 	if err != nil {
-			return err
+		return err
 	}
 	_ = ctx.GetStub().PutState(id, userAsBytes)
 	return nil
@@ -65,10 +65,10 @@ func (s *SmartContract) Register(ctx contractapi.TransactionContextInterface, na
 
 func (s *SmartContract) MakeBank(ctx contractapi.TransactionContextInterface, money string) error {
 	moneyAsInt, _ := strconv.Atoi(money)
-	strongBox := StrongBox{Money : moneyAsInt}
+	strongBox := StrongBox{Money: moneyAsInt}
 	boxAsBytes, err := json.Marshal(strongBox)
 	if err != nil {
-			log.Fatal(err)
+		log.Fatal(err)
 	}
 	return ctx.GetStub().PutState("bank", boxAsBytes)
 }
@@ -76,16 +76,16 @@ func (s *SmartContract) MakeBank(ctx contractapi.TransactionContextInterface, mo
 func (s *SmartContract) TurnRoulette(ctx contractapi.TransactionContextInterface, money string, id, box string) (int, error) {
 	userAsBytes, err := ctx.GetStub().GetState(id)
 	if err != nil {
-			return -1, err
+		return -1, err
 	}
 	user := new(User)
 	_ = json.Unmarshal(userAsBytes, user)
 	if isTrueUserBan(user) == true {
-			return -1, fmt.Errorf("룰렛 금지")
+		return -1, fmt.Errorf("룰렛 금지")
 	}
 	boxAsBytes, err := ctx.GetStub().GetState(box)
 	if err != nil {
-			return -1, err
+		return -1, err
 	}
 	strongBox := new(StrongBox)
 	_ = json.Unmarshal(boxAsBytes, strongBox)
@@ -95,21 +95,21 @@ func (s *SmartContract) TurnRoulette(ctx contractapi.TransactionContextInterface
 	roulette := 100 - user.LuckyScore
 	randomValue := rand.Intn(roulette)
 	if randomValue == 1 {
-			user.LuckyScore = 0
-			strongBox.Money -= user.LuckyScore * 2
-			user.Money += user.LuckyScore * 2
-			userAsBytes, err = json.Marshal(user)
-			boxAsBytes, err = json.Marshal(strongBox)
-			_ = ctx.GetStub().PutState(box, boxAsBytes)
-			_ = ctx.GetStub().PutState(id, userAsBytes)
-			return user.LuckyScore * 2, nil
+		user.LuckyScore = 0
+		strongBox.Money -= user.LuckyScore * 2
+		user.Money += user.LuckyScore * 2
+		userAsBytes, err = json.Marshal(user)
+		boxAsBytes, err = json.Marshal(strongBox)
+		_ = ctx.GetStub().PutState(box, boxAsBytes)
+		_ = ctx.GetStub().PutState(id, userAsBytes)
+		return user.LuckyScore * 2, nil
 	} else {
-			user.LuckyScore += 5
-			userAsBytes, err = json.Marshal(user)
-			boxAsBytes, err = json.Marshal(strongBox)
-			_ = ctx.GetStub().PutState(box, boxAsBytes)
-			_ =  ctx.GetStub().PutState(id, userAsBytes)
-			return 0, nil
+		user.LuckyScore += 5
+		userAsBytes, err = json.Marshal(user)
+		boxAsBytes, err = json.Marshal(strongBox)
+		_ = ctx.GetStub().PutState(box, boxAsBytes)
+		_ = ctx.GetStub().PutState(id, userAsBytes)
+		return 0, nil
 	}
 }
 
@@ -118,55 +118,55 @@ func (s *SmartContract) QueryAllUser(ctx contractapi.TransactionContextInterface
 	resultsIterator, err := ctx.GetStub().GetStateByRange(startKey, endKey)
 
 	if err != nil {
-			return nil, err
+		return nil, err
 	}
 	defer resultsIterator.Close()
 
 	results := []QueryResult{}
 
 	for resultsIterator.HasNext() {
-			queryRes, err := resultsIterator.Next()
+		queryRes, err := resultsIterator.Next()
 
-			if err != nil {
-					return nil, err
-			}
+		if err != nil {
+			return nil, err
+		}
 
-			user := new(User)
-			_ = json.Unmarshal(queryRes.Value, user)
+		user := new(User)
+		_ = json.Unmarshal(queryRes.Value, user)
 
-			queryResult := QueryResult{Key: queryRes.Key, Record: user}
-			if user.Name != ""{
-				results = append(results, queryResult)
-			}
+		queryResult := QueryResult{Key: queryRes.Key, Record: user}
+		if user.Name != "" {
+			results = append(results, queryResult)
+		}
 	}
 
 	return results, nil
 }
 
-func (s *SmartContract) BorrowMoney(ctx contractapi.TransactionContextInterface, money string, id, box string) error {
+func (s *SmartContract) BorrowMoney(ctx contractapi.TransactionContextInterface, money string, id string) error {
 	userAsBytes, err := ctx.GetStub().GetState(id)
 	if err != nil {
-			return fmt.Errorf(err.Error())
+		return fmt.Errorf(err.Error())
 	}
 	user := new(User)
 	_ = json.Unmarshal(userAsBytes, user)
 	if isTrueUserBan(user) == true {
-			return fmt.Errorf("돈 못빌림")
+		return fmt.Errorf("돈 못빌림")
 	}
-	boxAsBytes, err := ctx.GetStub().GetState(box)
+	boxAsBytes, err := ctx.GetStub().GetState("bank")
 	if err != nil {
-			return fmt.Errorf(err.Error())
+		return fmt.Errorf(err.Error())
 	}
 	strongBox := new(StrongBox)
 	_ = json.Unmarshal(boxAsBytes, strongBox)
 	moneyAsInt, _ := strconv.Atoi(money)
 	if moneyAsInt > strongBox.Money {
-			return fmt.Errorf("요청한 돈이 매우 큼")
+		return fmt.Errorf("요청한 돈이 매우 큼")
 	}
 	user.Money += moneyAsInt
 	user.Warning--
 	boxAsBytes, err = json.Marshal(strongBox)
-	_ = ctx.GetStub().PutState(box, boxAsBytes)
+	_ = ctx.GetStub().PutState("bank", boxAsBytes)
 	userAsBytes, err = json.Marshal(user)
 	return ctx.GetStub().PutState(id, userAsBytes)
 }
@@ -174,11 +174,11 @@ func (s *SmartContract) BorrowMoney(ctx contractapi.TransactionContextInterface,
 func main() {
 	chaincode, err := contractapi.NewChaincode(new(SmartContract))
 	if err != nil {
-			fmt.Println(err.Error())
-			return
+		fmt.Println(err.Error())
+		return
 	}
 
 	if err = chaincode.Start(); err != nil {
-			fmt.Println(err.Error())
+		fmt.Println(err.Error())
 	}
 }
